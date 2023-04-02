9c9a43bacca04df26136d7dc8a06abfad0a81c24
yamaimo <hello@yamaimo.dev>
2023-04-03 00:49:28 +0900

    action.pyにdocstringを追加

--- chap3/action.py
+++ chap3/action.py
@@ -10,15 +10,18 @@ ACTION_KIND_ASK = "ask"
 
 
 def AskAction_init(card: Card) -> AskAction:
+    """質問を生成して返す"""
     data = (ACTION_KIND_ASK, card)
     return AskAction(data)
 
 
 def AskAction_get_card(action: AskAction) -> Card:
+    """質問したカードを返す"""
     return action[1]
 
 
 def AskAction_is_hit(action: AskAction, hand: Hand) -> bool:
+    """質問したカードが手札にあるか返す"""
     return Hand_has_card(hand, action[1])  # type: ignore
 
 
@@ -30,17 +33,20 @@ ACTION_KIND_GUESS = "guess"
 
 
 def GuessAction_init(card: Card) -> GuessAction:
+    """推測を生成して返す"""
     data = (ACTION_KIND_GUESS, card)
     return GuessAction(data)
 
 
 def GuessAction_get_card(action: GuessAction) -> Card:
+    """推測したカードを返す"""
     return action[1]
 
 
 def GuessAction_is_hit(
     action: GuessAction, rest_card: Card
 ) -> bool:
+    """推測したカードが残りのカードと一致するか返す"""
     return action[1] == rest_card  # type: ignore
 
 
@@ -50,10 +56,12 @@ Action = Union[AskAction, GuessAction]
 
 
 def Action_is_ask(action: Action) -> bool:
+    """行動が質問かを返す"""
     return action[0] == ACTION_KIND_ASK
 
 
 def Action_is_guess(action: Action) -> bool:
+    """行動が推測かを返す"""
     return action[0] == ACTION_KIND_GUESS
 
 
@@ -68,37 +76,46 @@ def ActionList_init(
     ask_actions: list[AskAction],
     guess_actions: list[GuessAction],
 ) -> ActionList:
+    """行動の一覧を生成して返す"""
     data = (ask_actions, guess_actions)
     return ActionList(data)
-
-
-def ActionList_get_all_actions(
-    action_list: ActionList,
-) -> list[Action]:
-    return action_list[0] + action_list[1]  # type: ignore
 
 
 def ActionList_get_ask_actions(
     action_list: ActionList,
 ) -> list[AskAction]:
+    """質問の一覧を返す"""
     return action_list[0]
 
 
 def ActionList_get_guess_actions(
     action_list: ActionList,
 ) -> list[GuessAction]:
+    """推測の一覧を返す"""
     return action_list[1]
+
+
+def ActionList_get_all_actions(
+    action_list: ActionList,
+) -> list[Action]:
+    """行動の一覧を返す"""
+    return action_list[0] + action_list[1]  # type: ignore
 
 
 def ActionList_contains_action(
     action_list: ActionList, action: Action
 ) -> bool:
+    """指定された行動が一覧に含まれるか返す"""
     return action in ActionList_get_all_actions(action_list)
 
 
 def ActionList_get_available_actions(
     hand: Hand, prev_action: Optional[AskAction]
 ) -> ActionList:
+    """
+    手番プレイヤーの手札と直前の行動から
+    選択可能な行動の一覧を生成して返す
+    """
     ask_actions = [
         AskAction_init(card) for card in Card_get_all_cards()
     ]
