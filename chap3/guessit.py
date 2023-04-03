36e21d5afb5752ae8de9fe9c7e74324cb58172be
yamaimo <hello@yamaimo.dev>
2023-04-04 00:45:20 +0900

    guessit.pyを少し修正

--- chap3/guessit.py
+++ chap3/guessit.py
@@ -9,20 +9,27 @@ from player import (
 )
 from terminal import Terminal_init, Terminal_put_str
 
-dealer = Dealer_init()
-deal = Dealer_deal(dealer)
 
-terminal = Terminal_init()
-human = HumanPlayer_init(
-    "You", Deal_get_player0_hand(deal), terminal
-)
-ai = RandomAI_init("AI")
+def main() -> None:
+    """メイン"""
+    dealer = Dealer_init()
+    deal = Dealer_deal(dealer)
 
-player0 = Player_init_with_human_player(human)
-player1 = Player_init_with_random_ai(ai)
+    terminal = Terminal_init()
+    human = HumanPlayer_init(
+        "Player0", Deal_get_player0_hand(deal), terminal
+    )
+    ai = RandomAI_init("Player1")
 
-game = Game_init(deal, player0, player1, terminal)
-win_player = Game_start(game)
+    player0 = Player_init_with_human_player(human)
+    player1 = Player_init_with_random_ai(ai)
 
-win_player_name = Player_get_name(win_player)
-Terminal_put_str(terminal, f"{win_player_name} won.")
+    game = Game_init(deal, player0, player1, terminal)
+    win_player = Game_start(game)
+
+    win_player_name = Player_get_name(win_player)
+    Terminal_put_str(terminal, f"{win_player_name} won.")
+
+
+if __name__ == "__main__":
+    main()
