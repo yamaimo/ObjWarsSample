320a6205a17f630546f6990987c00da6450a089a
yamaimo <hello@yamaimo.dev>
2023-04-03 09:18:43 +0900

    terminal.pyにdocstringを追加

--- chap3/terminal.py
+++ chap3/terminal.py
@@ -10,23 +10,30 @@ def Terminal_init(
     in_stream: TextIO = sys.stdin,
     out_stream: TextIO = sys.stdout,
 ) -> Terminal:
+    """ターミナルを生成して返す"""
     data = (in_stream, out_stream)
     return Terminal(data)
 
 
 def Terminal_get_str(terminal: Terminal, prompt: str) -> str:
+    """
+    プロンプトを表示し、
+    入力された文字列を1行読み取って返す
+    """
     terminal[1].write(prompt)
     terminal[1].flush()
     return terminal[0].readline()
 
 
 def Terminal_put_str(terminal: Terminal, output: str) -> None:
+    """文字列を出力する"""
     terminal[1].write(output)
     terminal[1].write("\n")
     terminal[1].flush()
 
 
 def Terminal_put_empty_line(terminal: Terminal) -> None:
+    """空行を出力する"""
     terminal[1].write("\n")
     terminal[1].flush()
 
